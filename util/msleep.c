b89822452c86fe69e25351e80f86c5bf0fc78f28
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-09-26 18:02:17 -0700

    Add util/msleep
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

---
+++ util/msleep.c
@@ -0,0 +1,17 @@
+/*
+ * The OneFish distributed filesystem
+ *
+ * Copyright 2011, Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
+ *
+ * This is licensed under the Apache License, Version 2.0.  See file COPYING.
+ */
+
+#include "util/msleep.h"
+
+#include <poll.h>
+#include <stdlib.h>
+
+void do_msleep(int milli)
+{
+	poll(NULL, 0, milli);
+}
---
+++ util/msleep.h
@@ -0,0 +1,19 @@
+/*
+ * The OneFish distributed filesystem
+ *
+ * Copyright 2011, Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
+ *
+ * This is licensed under the Apache License, Version 2.0.  See file COPYING.
+ */
+
+#ifndef ONEFISH_UTIL_SLEEP_DOT_H
+#define ONEFISH_UTIL_SLEEP_DOT_H
+
+/** Sleep for a given number of milliseconds.
+ * Unlike sleep(3), this is guaranteed not to use signals.
+ *
+ * @param milli		Number of milliseconds to sleep
+ */
+void do_msleep(int milli);
+
+#endif
