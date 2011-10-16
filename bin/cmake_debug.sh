43639d320359f40c89c521335d3d0500359eb12a
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-10-16 11:23:29 -0700

    bin/cmake_debug.sh: make it a little more convenient
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- bin/cmake_debug.sh
+++ bin/cmake_debug.sh
@@ -3,8 +3,12 @@
 SNAME="`readlink -f $0`"
 DIRNAME="`dirname "$SNAME"`"
 
+# FIXME: remove REUSEADDR_HACK
+
 cmake \
 -DCMAKE_BUILD_TYPE=Debug \
+-DREDFISH_CLIENT_LIB=fishc_stub \
+-DREDFISH_SO_REUSEADDR_HACK=1 \
 -DCTEST_MEMORYCHECK_COMMAND:FILEPATH=/usr/bin/valgrind \
 -DCTEST_MEMORYCHECK_COMMAND_OPTIONS:STRING="--trace-children=yes \
 --quiet --tool=memcheck --leak-check=yes --show-reachable=yes \
