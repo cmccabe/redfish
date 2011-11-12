9941546977ad7b61b1a42c14d60a452a6d4208e4
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-11-12 11:48:46 -0800

    Add mds/limits.h
    Defines some limits for the MDS
    
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

---
+++ mds/limits.h
@@ -0,0 +1,24 @@
+/*
+ * The RedFish distributed filesystem
+ *
+ * Copyright (C) 2011 Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
+ *
+ * This is licensed under the Apache License, Version 2.0.  See file COPYING.
+ */
+
+#ifndef REDFISH_MDS_LIMITS_DOT_H
+#define REDFISH_MDS_LIMITS_DOT_H
+
+/** maximum length of a path name in RedFish */
+#define RF_PATH_MAX 4096
+
+/** maximum number of MDSes in a cluster */
+#define MAX_MDS 64
+
+/** Maximum length of a user name */
+#define USER_MAX 64
+
+/** Maximum length of a group name */
+#define GROUP_MAX 64
+
+#endif
