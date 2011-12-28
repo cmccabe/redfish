4fd437657aa6ea315274d914ab9d2d25c0479e69
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-12-28 11:14:30 -0800

    udata: implement numeric user IDs, groups
    We already have to centrally manage the mapping between users and
    groups.  So while we're at it, we can store a mapping between textual
    and numeric user IDs.  Also implement serialization / deserialization.
    
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- mds/user.c
+++ mds/user.c
@@ -9,34 +9,314 @@
 #include "mds/limits.h"
 #include "mds/user.h"
 #include "util/compiler.h"
+#include "util/error.h"
+#include "util/packed.h"
+#include "util/string.h"
 
+#include <errno.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
+PACKED(
+struct packed_udata {
+	uint32_t num_user;
+	uint32_t num_group;
+	/* users */
+	/* groups */
+});
+
+PACKED(
+struct packed_user {
+	/** user ID */
+	uint32_t uid;
+	/** primary group ID */
+	uint32_t gid;
+	/** number of secondary groups */
+	uint32_t num_segid;
+	/** secondary groups */
+	char data[0];
+	/** user name */
+});
+
+PACKED(
+struct packed_group {
+	/** group ID */
+	uint32_t gid;
+	/** group name */
+	char data[0];
+});
+
+static int user_compare(struct user *a, struct user *b)
+{
+	return strcmp(a->name, b->name);
+}
+
+static int group_compare(struct group *a, struct group *b)
+{
+	return strcmp(a->name, b->name);
+}
+
+RB_HEAD(users, user);
+RB_GENERATE(users, user, entry, user_compare);
+
+RB_HEAD(groups, group);
+RB_GENERATE(groups, group, entry, group_compare);
+
 struct udata {
-	int i;
+	/** All users, sorted by ID */
+	struct users users_head;
+	/** All groups, sorted by ID */
+	struct groups groups_head;
 };
-static struct udata blah;
 
-struct udata *udata_create(void)
+struct udata *udata_alloc(void)
 {
-	// TODO: use fgetpwent or similar to find system users, and store the
-	// information here.
-	return (struct udata*)&blah;
+	struct udata *udata;
+
+	udata = calloc(1, sizeof(struct udata));
+	if (!udata)
+		return NULL;
+	RB_INIT(&udata->users_head);
+	RB_INIT(&udata->groups_head);
+	return udata;
 }
 
-void udata_free(POSSIBLY_UNUSED(struct udata *udata))
+void udata_free(struct udata *udata)
 {
-	// do nothing
+	struct user *u, *u_tmp;
+	struct group *g, *g_tmp;
+
+	RB_FOREACH_SAFE(u, users, &udata->users_head, u_tmp) {
+		RB_REMOVE(users, &udata->users_head, u);
+		free(u);
+	}
+	RB_FOREACH_SAFE(g, groups, &udata->groups_head, g_tmp) {
+		RB_REMOVE(groups, &udata->groups_head, g);
+		free(g);
+	}
+	free(udata);
 }
 
-int user_in_group(POSSIBLY_UNUSED(const struct udata *udata),
-		const char *user, const char *group)
+int user_in_gid(const struct user *user, uint32_t gid)
 {
-	return (!strcmp(user, group));
+	size_t i;
+	uint32_t num_segid;
+
+	if (user->gid == gid)
+		return 1;
+	num_segid = user->num_segid;
+	for (i = 0; i < num_segid; ++i) {
+		if (user->segid[i] == gid)
+			return 1;
+	}
+	return 0;
 }
 
-const char *user_primary_group(POSSIBLY_UNUSED(const struct udata *udata),
-		const char *user)
+const struct user *udata_lookup_user(struct udata *udata,
+		const char *name)
 {
+	struct user *user, exemplar;
+
+	memset(&exemplar, 0, sizeof(exemplar));
+	if (zsnprintf(exemplar.name, RF_USER_MAX, "%s", name))
+		return ERR_PTR(ENAMETOOLONG);
+	user = RB_FIND(users, &udata->users_head, &exemplar);
+	if (!user)
+		return ERR_PTR(ENOENT);
 	return user;
+}
+
+const struct group *udata_lookup_group(struct udata *udata,
+		const char *name)
+{
+	struct group *group, exemplar;
+
+	memset(&exemplar, 0, sizeof(exemplar));
+	if (zsnprintf(exemplar.name, RF_USER_MAX, "%s", name))
+		return ERR_PTR(ENAMETOOLONG);
+	group = RB_FIND(groups, &udata->groups_head, &exemplar);
+	if (!group)
+		return ERR_PTR(ENOENT);
+	return group;
+}
+
+int pack_user(const struct user *user, char *buf,
+		uint32_t *off, uint32_t max)
+{
+	int ret;
+	uint32_t o, need, num_segid, i;
+	struct packed_user *hdr;
+
+	o = *off;
+	need = sizeof(struct packed_user) +
+			 (sizeof(uint32_t) * user->num_segid);
+	if (max - o < need)
+		return -ENAMETOOLONG;
+	hdr = (struct packed_user*)(buf + o);
+	o += need;
+	pack_to_be32(&hdr->uid, user->uid);
+	pack_to_be32(&hdr->gid, user->gid);
+	num_segid = user->num_segid;
+	pack_to_be32(&hdr->num_segid, num_segid);
+	for (i = 0; i < num_segid; ++i) {
+		pack_to_be32(hdr->data + o, user->segid[i]);
+		o += sizeof(uint32_t);
+	}
+	ret = pack_str(buf, &o, max, user->name);
+	if (ret)
+		return ret;
+	*off = o;
+	return 0;
+}
+
+int pack_group(const struct group *group, char *buf,
+		uint32_t *off, uint32_t max)
+{
+	int ret;
+	uint32_t o, need;
+	struct packed_group *hdr;
+
+	o = *off;
+	need = sizeof(struct packed_user);
+	if (max - o < need)
+		return -ENAMETOOLONG;
+	hdr = (struct packed_group*)(buf + o);
+	o += need;
+	pack_to_be32(&hdr->gid, group->gid);
+	ret = pack_str(buf, &o, max, group->name);
+	if (ret)
+		return ret;
+	*off = o;
+	return 0;
+}
+
+int pack_udata(struct udata *udata, char *buf,
+		uint32_t *off, uint32_t max)
+{
+	int ret;
+	uint32_t o, need, num_user = 0, num_group = 0;
+	struct packed_udata *hdr;
+	struct user *u;
+	struct group *g;
+
+	o = *off;
+	need = sizeof(struct packed_udata);
+	if (max - o < need)
+		return -ENAMETOOLONG;
+	hdr = (struct packed_udata*)(buf + o);
+	o += need;
+	RB_FOREACH(u, users, &udata->users_head) {
+		ret = pack_user(u, buf, &o, max);
+		if (ret)
+			return ret;
+		num_user++;
+	}
+	RB_FOREACH(g, groups, &udata->groups_head) {
+		ret = pack_group(g, buf, &o, max);
+		if (ret)
+			return ret;
+		num_group++;
+	}
+	pack_to_be32(&hdr->num_user, num_user);
+	pack_to_be32(&hdr->num_group, num_group);
+	*off = o;
+	return 0;
+}
+
+struct user *unpack_user(char *buf, uint32_t *off, uint32_t max)
+{
+	int ret;
+	uint32_t o, num_segid, i;
+	struct packed_user *hdr;
+	struct user *user;
+
+	o = *off;
+	if (max - o < sizeof(struct packed_user))
+		return ERR_PTR(ENAMETOOLONG);
+	hdr = (struct packed_user*)(buf + o);
+	o += sizeof(struct packed_user);
+	num_segid = unpack_from_be32(&hdr->num_segid);
+	if (max - o < sizeof(uint32_t) * num_segid)
+		return ERR_PTR(ENAMETOOLONG);
+	user = calloc(1, sizeof(struct user) + (sizeof(uint32_t) * num_segid));
+	if (!user)
+		return ERR_PTR(ENOMEM);
+	user->uid = unpack_from_be32(&hdr->uid);
+	user->gid = unpack_from_be32(&hdr->gid);
+	user->num_segid = num_segid;
+	ret = unpack_str(buf, &o, max, user->name, RF_USER_MAX);
+	if (ret)
+		return ERR_PTR(FORCE_POSITIVE(ret));
+	for (i = 0; i < num_segid; ++i) {
+		user->segid[i] = unpack_from_be32(buf + o);
+		o += sizeof(uint32_t);
+	}
+	*off = o;
+	return user;
+}
+
+struct group *unpack_group(char *buf, uint32_t *off, uint32_t max)
+{
+	int ret;
+	uint32_t o;
+	struct packed_group *hdr;
+	struct group *group;
+	char name[RF_GROUP_MAX];
+
+	o = *off;
+	if (max - o < sizeof(struct packed_group))
+		return ERR_PTR(ENAMETOOLONG);
+	hdr = (struct packed_group*)(buf + o);
+	o += sizeof(struct packed_group);
+	ret = unpack_str(buf, &o, max, name, RF_GROUP_MAX);
+	if (ret)
+		return ERR_PTR(FORCE_POSITIVE(ret));
+	group = malloc(sizeof(struct group) + strlen(name) + 1);
+	if (!group)
+		return ERR_PTR(ENOMEM);
+	group->gid = unpack_from_be32(&hdr->gid);
+	ret = zsnprintf(group->name, RF_GROUP_MAX, "%s", name);
+	if (ret)
+		return ERR_PTR(FORCE_POSITIVE(ret));
+	return group;
+}
+
+struct udata* unpack_udata(char *buf, uint32_t *off, uint32_t max)
+{
+	uint32_t o, need, num_user, num_group, i;
+	struct packed_udata *hdr;
+	struct udata *udata;
+	struct group *g;
+	struct user *u;
+
+	o = *off;
+	need = sizeof(struct packed_udata);
+	if (max - o < need)
+		return ERR_PTR(ENAMETOOLONG);
+	udata = udata_alloc();
+	if (!udata)
+		return ERR_PTR(ENOMEM);
+	hdr = (struct packed_udata*)(buf + o);
+	o += need;
+	num_user = unpack_from_be32(&hdr->num_user);
+	num_group = unpack_from_be32(&hdr->num_group);
+	for (i = 0; i < num_user; ++i) {
+		u = unpack_user(buf, &o, max);
+		if (IS_ERR(u)) {
+			udata_free(udata);
+			return (struct udata*)u;
+		}
+		RB_INSERT(users, &udata->users_head, u);
+	}
+	for (i = 0; i < num_group; ++i) {
+		g = unpack_group(buf, &o, max);
+		if (IS_ERR(g)) {
+			udata_free(udata);
+			return (struct udata*)g;
+		}
+		RB_INSERT(groups, &udata->groups_head, g);
+	}
+	return udata;
 }
--- mds/user.h
+++ mds/user.h
@@ -9,11 +9,37 @@
 #ifndef REDFISH_MDS_USER_DOT_H
 #define REDFISH_MDS_USER_DOT_H
 
-/** Create a user data lookup cache
+#include <stdint.h> /* for uint32_t, etc. */
+
+#include "util/tree.h" /* for RB_ENTRY */
+
+struct user {
+	RB_ENTRY(user) entry;
+	/** user ID */
+	uint32_t uid;
+	/** user name */
+	char name[RF_USER_MAX];
+	/** primary group ID */
+	uint32_t gid;
+	/** number of secondary groups */
+	uint32_t num_segid;
+	/** secondary groups */
+	uint32_t segid[0];
+};
+
+struct group {
+	RB_ENTRY(group) entry;
+	/** group ID */
+	uint32_t gid;
+	/** group name */
+	char name[0];
+};
+
+/** Allocate an empty user data lookup cache
  *
  * @return		The user data, or an error pointer on failure
  */
-extern struct udata *udata_create(void);
+extern struct udata *udata_alloc(void);
 
 /** Free a user data lookup cache
  *
@@ -23,23 +49,97 @@ extern void udata_free(struct udata *udata);
 
 /** Determine if a user is in a group
  *
- * @param udata		The user data
  * @param user		The user
+ * @param gid		The group id
  *
  * @return		0 if the user is not in the group; 1 otherwise
  */
-extern int user_in_group(const struct udata *udata,
-		const char *user, const char *group);
+extern int user_in_gid(const struct user *user, uint32_t gid);
 
-/** Find the primary group of a user
+/** Get information about a user given his name
  *
  * @param udata		The user data
- * @param user		The user
+ * @param name		The user name
  *
- * @return		The user's primary group
- *			NULL if the user is unknown
+ * @return		The user data, or an error pointer
  */
-extern const char *user_primary_group(const struct udata *udata,
-		const char *user);
+extern const struct user *udata_lookup_user(struct udata *udata,
+		const char *name);
+
+/** Get information about a group given its name
+ *
+ * @param udata		The user data
+ * @param name		The group name
+ *
+ * @return		The group data, or an error pointer
+ */
+extern const struct group *udata_lookup_group(struct udata *udata,
+		const char *name);
+
+/** Pack user information to a byte buffer
+ *
+ * @param user		The user
+ * @param buf		The buffer to pack to
+ * @param off		(inout) offset in the buffer to pack to
+ * @param max		size of the buffer to pack to
+ *
+ * @return		0 on success; error code otherwise
+ */
+extern int pack_user(const struct user *user, char *buf,
+		uint32_t *off, uint32_t max);
+
+/** Pack group information to a byte buffer
+ *
+ * @param group		The group
+ * @param buf		The buffer to pack to
+ * @param off		(inout) offset in the buffer to pack to
+ * @param max		size of the buffer to pack to
+ *
+ * @return		0 on success; error code otherwise
+ */
+int pack_group(const struct group *group, char *buf,
+		uint32_t *off, uint32_t max);
+
+/** Pack userdata to a byte buffer
+ *
+ * @param udata		The userdata
+ * @param buf		The buffer to pack to
+ * @param off		(inout) offset in the buffer to pack to
+ * @param max		size of the buffer to pack to
+ *
+ * @return		0 on success; error code otherwise
+ */
+int pack_udata(struct udata *udata, char *buf,
+		uint32_t *off, uint32_t max);
+
+/** Unpack user information from a byte buffer
+ *
+ * @param buf		The buffer to unpack from
+ * @param off		(inout) offset in the buffer to unpack from
+ * @param max		size of the buffer to unpack from
+ *
+ * @return		the dynamically allocated user data, or an error ptr
+ */
+struct user *unpack_user(char *buf, uint32_t *off, uint32_t max);
+
+/** Unpack group information from a byte buffer
+ *
+ * @param buf		The buffer to unpack from
+ * @param off		(inout) offset in the buffer to unpack from
+ * @param max		size of the buffer to unpack from
+ *
+ * @return		the dynamically allocated group data, or an error ptr
+ */
+struct group *unpack_group(char *buf, uint32_t *off, uint32_t max);
+
+/** Unpack userdata information from a byte buffer
+ *
+ * @param buf		The buffer to unpack from
+ * @param off		(inout) offset in the buffer to unpack from
+ * @param max		size of the buffer to unpack from
+ *
+ * @return		the dynamically allocated userdata, or an error ptr
+ */
+struct udata* unpack_udata(char *buf, uint32_t *off, uint32_t max);
 
 #endif
