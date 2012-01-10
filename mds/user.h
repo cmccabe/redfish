2de5af28a2762b5285d34ed3439d3484d675c610
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2012-01-09 20:52:48 -0800

    spruce up file headers a bit
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- mds/user.h
+++ mds/user.h
@@ -1,9 +1,17 @@
 /*
- * The RedFish distributed filesystem
+ * Copyright 2011-2012 the RedFish authors
  *
- * Copyright (C) 2011 Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This is licensed under the Apache License, Version 2.0.  See file COPYING.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef REDFISH_MDS_USER_DOT_H
