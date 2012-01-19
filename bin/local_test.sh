c3360dc19ef0693dc018e98495c1a0f5364c250b
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2012-01-19 05:44:19 -0800

    Add bin/local_test.sh script
    A little script to help test the stub client library.
    
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

---
+++ bin/local_test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright 2012 the RedFish authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# local_test.sh
+#
+# Run some local tests with the stub client library.
+# This should be run from the CMake build directory.
+#
+
+die() {
+	echo -n "failed: "
+	echo $@
+	exit 1
+}
+
+export STUB_BASE=`mktemp -d -t local_test.XXXXXXXXXX`
+trap "[ -n $SKIP_CLEANUP ] && rm -rf ${STUB_BASE}; exit" INT TERM EXIT
+echo "Tests running with STUB_BASE=${STUB_BASE}"
+
+./stest/st_startup -f || die "line ${LINENO}"
+./stest/st_trivial -f || die "line ${LINENO}"
+./stest/st_write_then_read -f || die "line ${LINENO}"
+
+echo "*** SUCCESS ***"
+exit 0
