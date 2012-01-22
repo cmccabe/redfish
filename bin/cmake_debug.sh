9006253156d00a0d8bbd2d78bbbe6086fd299266
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2012-01-22 15:17:41 -0800

    CMakeLists: add REDFISH_BUILD_JNI option
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- bin/cmake_debug.sh
+++ bin/cmake_debug.sh
@@ -9,6 +9,7 @@ cmake \
 -DCMAKE_BUILD_TYPE=Debug \
 -DREDFISH_CLIENT_LIB=fishc_stub \
 -DREDFISH_SO_REUSEADDR_HACK:BOOL=ON \
+-DREDFISH_BUILD_JNI:BOOL=ON \
 -DCTEST_MEMORYCHECK_COMMAND:FILEPATH=/usr/bin/valgrind \
 -DCTEST_MEMORYCHECK_COMMAND_OPTIONS:STRING="--trace-children=yes \
 --quiet --tool=memcheck --leak-check=yes --show-reachable=yes \
