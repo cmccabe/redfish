9368bce7cf1436267d7cb5505e84220a8aff26ef
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2012-01-30 15:39:57 -0800

    hadoop: TestRedfishFileSystem:fix and improve test
    Fix a lot of problems in the test (trying to write a "byte" that didn't
    fit in one byte, nonstandard indendation, screwed up directory
    structure, and failing to clean up after itself).
    Test passes now...
    
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- hadoop/test/TestRedfishFileSystem.java
+++ hadoop/test/TestRedfishFileSystem.java
@@ -20,6 +20,8 @@ import java.io.*;
 import java.net.*;
 import junit.framework.TestCase;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
@@ -31,6 +33,9 @@ import org.apache.hadoop.fs.redfish.RedfishFileSystem;
 
 public class TestRedfishFileSystem extends TestCase {
   private RedfishFileSystem m_fs;
+  public static final Log LOG = LogFactory.getLog(
+      "org.apache.hadoop.fs.redfish.TestRedfishFileSystem");
+  private final Path m_testBase = new Path("tbase");
 
   @Override
   protected void setUp() throws IOException {
@@ -40,6 +45,8 @@ public class TestRedfishFileSystem extends TestCase {
     conf.set("fs.redfish.configFile", "/media/fish/redfish/conf/local.conf");
     m_fs = new RedfishFileSystem();
     m_fs.initialize(URI.create("redfish:///"), conf);
+    m_fs.delete(m_testBase, true);
+    m_fs.mkdirs(m_testBase);
   }
 
   @Override
@@ -50,75 +57,81 @@ public class TestRedfishFileSystem extends TestCase {
   // @Test
   // Test mkdir and recursive rm
   public void testMkdir() throws Exception {
-      // make the dir
-      Path d1 = new Path("d1");
-      m_fs.mkdirs(d1);
-      assertTrue(m_fs.isDirectory(d1));
-      m_fs.setWorkingDirectory(d1);
+    // make the dir
+    Path d1 = new Path(m_testBase, "d1");
+    m_fs.mkdirs(d1);
+    assertTrue(m_fs.isDirectory(d1));
+    m_fs.setWorkingDirectory(d1);
 
-      Path d2 = new Path("d2");
-      m_fs.mkdirs(d2);
-      assertTrue(m_fs.isDirectory(d2));
+    Path d2 = new Path("d2");
+    m_fs.mkdirs(d2);
+    assertTrue(m_fs.isDirectory(d2));
+    m_fs.setWorkingDirectory(new Path(".."));
+    assertTrue(m_fs.isDirectory(new Path("d1/d2")));
+    assertFalse(m_fs.isDirectory(new Path("d1/nonexistent")));
 
-      assertFalse(m_fs.isDirectory(new Path("/d1")));
-      assertFalse(m_fs.isDirectory(new Path("/d1/d2")));
+    FileStatus[] p = m_fs.listStatus(new Path("."));
+    assertEquals(1, p.length);
 
-      FileStatus[] p = m_fs.listStatus(d1);
-      assertEquals(p.length, 1);
-
-      m_fs.delete(d1, true);
-      assertFalse(m_fs.exists(d1));
+    m_fs.delete(d1, true);
+    assertFalse(m_fs.exists(d1));
   }
 
   // @Test
   // Test file creation and unlinking
   public void testUnlink() throws Exception {
-      Path sft = new Path("/sft");
-      m_fs.mkdirs(sft);
-      assertTrue(m_fs.isDirectory(sft));
+    Path sft = new Path(m_testBase, "sft");
+    m_fs.mkdirs(sft);
+    assertTrue(m_fs.isDirectory(sft));
 
-      Path foo1 = new Path("/sft/foo.1");
-      FSDataOutputStream foo1out = m_fs.create(foo1, true, 4096, (short) 1, (long) 4096, null);
-      foo1out.close();
-      FileStatus[] p = m_fs.listStatus(sft);
-      assertEquals(p.length, 1);
+    Path foo1 = new Path(sft, "foo.1");
+    FSDataOutputStream foo1out = m_fs.create(foo1, true, 4096, (short) 1, (long) 4096, null);
+    assertNotSame(foo1out, null);
+    foo1out.close();
+    FileStatus[] p = m_fs.listStatus(sft);
+    assertNotSame(null, p);
+    assertEquals(1, p.length);
 
-      Path foo2 = new Path("/sft/foo.2");
-      FSDataOutputStream foo2out = m_fs.create(foo2, true, 4096, (short) 1, (long) 4096, null);
-      foo2out.close();
-      p = m_fs.listStatus(sft);
-      assertEquals(p.length, 2);
+    Path foo2 = new Path(sft, "foo.2");
+    FSDataOutputStream foo2out = m_fs.create(foo2, true, 4096, (short) 1, (long) 4096, null);
+    assertNotSame(null, foo2out);
+    foo2out.close();
+    p = m_fs.listStatus(sft);
+    assertNotSame(null, p);
+    assertEquals(2, p.length);
 
-      m_fs.delete(foo1, true);
-      p = m_fs.listStatus(sft);
-      assertEquals(p.length, 1);
-      m_fs.delete(foo2, true);
-      p = m_fs.listStatus(sft);
-      assertEquals(p.length, 0);
-      m_fs.delete(sft, true);
-      assertFalse(m_fs.exists(sft));
+    m_fs.delete(foo1, true);
+    p = m_fs.listStatus(sft);
+    assertNotSame(null, p);
+    assertEquals(1, p.length);
+    m_fs.delete(foo2, true);
+    p = m_fs.listStatus(sft);
+    assertNotSame(null, p);
+    assertEquals(0, p.length);
+    m_fs.delete(sft, true);
+    assertFalse(m_fs.exists(sft));
   }
 
   // @Test
   // Check file/read write
   public void testReadAndWrite() throws Exception {
-    Path rwPath = new Path("/rw1");
+    Path rwPath = new Path(m_testBase, "rw1");
     FSDataOutputStream w1 = m_fs.create(rwPath, true, 4096,
           (short) 1, (long) 4096, null);
     byte[] data = new byte[4096];
     for (int i = 0; i < data.length; i++)
         data[i] = (byte) (i % 10);
-    w1.write((int)1000);
-    w1.write((int)2000);
+    w1.write((int)250);
+    w1.write((int)123);
     w1.write(data, 0, data.length);
     w1.close();
 
     FSDataInputStream r1 = m_fs.open(rwPath, 4096);
     int w;
     w = r1.read();
-    assertEquals(w, 1000);
+    assertEquals(250, w);
     w = r1.read();
-    assertEquals(w, 2000);
+    assertEquals(123, w);
 
     long oldPos = r1.getPos();
     assertTrue(oldPos != 0);
@@ -129,7 +142,19 @@ public class TestRedfishFileSystem extends TestCase {
 
     final int TEST_OFF = 10;
     byte[] buf2 = new byte[TEST_OFF + data.length];
-    r1.read(oldPos, buf, TEST_OFF, buf.length);
+
+    /* test out-of-bounds handling */
+    boolean gotException = false;
+    try {
+      r1.read(oldPos, buf, TEST_OFF, data.length);
+    }
+    catch (java.lang.IndexOutOfBoundsException e) {
+      gotException = true;
+    }
+    assertTrue(gotException);
+
+    /* test pread */
+    r1.read(oldPos, buf2, TEST_OFF, data.length);
     for (int i = 0; i < data.length; i++)
         assertEquals(data[i], buf2[i + TEST_OFF]);
     r1.close();
