85e60bc8863238bb6637b7ac8597437f6038fbe2
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2012-02-07 23:32:29 -0800

    deploy: add test_kills.py
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

---
+++ deploy/test_kills.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import json
+import os
+import subprocess
+import sys
+import tempfile
+from of_daemon import *
+from of_util import *
+from optparse import OptionParser
+
+check_python_version()
+parser = OptionParser()
+(opts, args, jo) = parse_deploy_opts(parser)
+
+diter = DaemonIter.from_conf_object(jo, None)
+for d in diter:
+    if d.id.ty != DaemonId.MDS:
+        continue
+    d.check_mds_status()
